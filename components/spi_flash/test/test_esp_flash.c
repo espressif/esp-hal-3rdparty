espressif/esp-hal-3rdparty

commit 76ff41ad34fbe0f4c07da4076a0b060ac79f200b

esp_flash: fix set qe bit and write command issues
There used to be dummy phase before out phase in common command
transactions. This corrupts the data.

The code before never actually operate (clear) the QE bit, once it finds
the QE bit is set. It's hard to check whether the QE set/disable
functions work well.

This commit:

1. Cancel the dummy phase

2. Set and clear the QE bit according to chip settings, allowing tests
   for QE bits. However for some chips (Winbond for example), it's not
   forced to clear the QE bit if not able to.

3. Also refactor to allow chip_generic and other chips to share the same
   code to read and write qe bit; let common command and read command share
   configure_host_io_mode.

4. Rename read mode to io mode since maybe we will write data with quad
   mode one day.

components/spi_flash/spi_flash_chip_generic.c
@@ -144,7 +144,7 @@ esp_err_t spi_flash_chip_generic_read(esp_flash_t *chip, void *buffer, uint32_t 
 {
     esp_err_t err = ESP_OK;
     // Configure the host, and return
-    spi_flash_chip_generic_config_host_read_mode(chip);
+    spi_flash_chip_generic_config_host_io_mode(chip);
 
     while (err == ESP_OK && length > 0) {
         uint32_t read_len = MIN(length, chip->host->max_read_bytes);
@@ -277,7 +277,7 @@ esp_err_t spi_flash_chip_generic_wait_idle(esp_flash_t *chip, uint32_t timeout_m
     return (timeout_ms > 0) ?  ESP_OK : ESP_ERR_TIMEOUT;
 }
 
-esp_err_t spi_flash_chip_generic_config_host_read_mode(esp_flash_t *chip)
+esp_err_t spi_flash_chip_generic_config_host_io_mode(esp_flash_t *chip)
 {
     uint32_t dummy_cyclelen_base;
     uint32_t addr_bitlen;
@@ -320,62 +320,33 @@ esp_err_t spi_flash_chip_generic_config_host_read_mode(esp_flash_t *chip)
         return ESP_ERR_FLASH_NOT_INITIALISED;
     }
 
-    return chip->host->configure_host_read_mode(chip->host, chip->read_mode, addr_bitlen, dummy_cyclelen_base, read_command);
+    return chip->host->configure_host_io_mode(chip->host, read_command, addr_bitlen, dummy_cyclelen_base,
+                                                chip->read_mode);
 }
 
-esp_err_t spi_flash_common_set_read_mode(esp_flash_t *chip, uint8_t qe_rdsr_command, uint8_t qe_wrsr_command, uint8_t qe_sr_bitwidth, unsigned qe_sr_bit)
-{
-    if (spi_flash_is_quad_mode(chip)) {
-        // Ensure quad modes are enabled, using the Quad Enable parameters supplied.
-        spi_flash_trans_t t = {
-            .command = qe_rdsr_command,
-            .mosi_data = 0,
-            .mosi_len = 0,
-            .miso_len = qe_sr_bitwidth,
-        };
-        chip->host->common_command(chip->host, &t);
-        unsigned sr = t.miso_data[0];
-        ESP_EARLY_LOGV(TAG, "set_read_mode: status before 0x%x", sr);
-        if ((sr & qe_sr_bit) == 0) {
-            //some chips needs the write protect to be disabled before writing to Status Register
-            chip->chip_drv->set_chip_write_protect(chip, false);
-
-            sr |= qe_sr_bit;
-            spi_flash_trans_t t = {
-                .command = qe_wrsr_command,
-                .mosi_data = sr,
-                .mosi_len = qe_sr_bitwidth,
-                .miso_len = 0,
-            };
-            chip->host->common_command(chip->host, &t);
-
-            /* Check the new QE bit has stayed set */
-            spi_flash_trans_t t_rdsr = {
-                .command = qe_rdsr_command,
-                .mosi_data = 0,
-                .mosi_len = 0,
-                .miso_len = qe_sr_bitwidth
-            };
-            chip->host->common_command(chip->host, &t_rdsr);
-            sr = t_rdsr.miso_data[0];
-            ESP_EARLY_LOGV(TAG, "set_read_mode: status after 0x%x", sr);
-            if ((sr & qe_sr_bit) == 0) {
-                return ESP_ERR_FLASH_NO_RESPONSE;
-            }
-
-            chip->chip_drv->set_chip_write_protect(chip, true);
-        }
-    }
-    return ESP_OK;
-}
-
-esp_err_t spi_flash_chip_generic_set_read_mode(esp_flash_t *chip)
+esp_err_t spi_flash_chip_generic_get_io_mode(esp_flash_t *chip, esp_flash_io_mode_t* out_io_mode)
 {
     // On "generic" chips, this involves checking
     // bit 1 (QE) of RDSR2 (35h) result
     // (it works this way on GigaDevice & Fudan Micro chips, probably others...)
     const uint8_t BIT_QE = 1 << 1;
-    return spi_flash_common_set_read_mode(chip, CMD_RDSR2, CMD_WRSR2, 8, BIT_QE);
+    uint32_t sr;
+    esp_err_t ret = spi_flash_common_read_status_8b_rdsr2(chip, &sr);
+    if (ret == ESP_OK) {
+        *out_io_mode = ((sr & BIT_QE)? SPI_FLASH_QOUT: 0);
+    }
+    return ret;
+}
+
+esp_err_t spi_flash_chip_generic_set_io_mode(esp_flash_t *chip)
+{
+    // On "generic" chips, this involves checking
+    // bit 9 (QE) of RDSR (05h) result
+    const uint32_t BIT_QE = 1 << 9;
+    return spi_flash_common_set_io_mode(chip,
+                                        spi_flash_common_write_status_16b_wrsr,
+                                        spi_flash_common_read_status_16b_rdsr_rdsr2,
+                                        BIT_QE);
 }
 
 static const char chip_name[] = "generic";
@@ -409,5 +380,134 @@ const spi_flash_chip_t esp_flash_chip_generic = {
     .write_encrypted = spi_flash_chip_generic_write_encrypted,
 
     .wait_idle = spi_flash_chip_generic_wait_idle,
-    .set_read_mode = spi_flash_chip_generic_set_read_mode,
+    .set_io_mode = spi_flash_chip_generic_set_io_mode,
+    .get_io_mode = spi_flash_chip_generic_get_io_mode,
 };
+
+/*******************************************************************************
+ * Utility functions
+ ******************************************************************************/
+
+static esp_err_t spi_flash_common_read_qe_sr(esp_flash_t *chip, uint8_t qe_rdsr_command, uint8_t qe_sr_bitwidth, uint32_t *sr)
+{
+    spi_flash_trans_t t = {
+        .command = qe_rdsr_command,
+        .mosi_data = 0,
+        .mosi_len = 0,
+        .miso_len = qe_sr_bitwidth,
+    };
+    esp_err_t ret = chip->host->common_command(chip->host, &t);
+    *sr = t.miso_data[0];
+    return ret;
+}
+
+static esp_err_t spi_flash_common_write_qe_sr(esp_flash_t *chip, uint8_t qe_wrsr_command, uint8_t qe_sr_bitwidth, uint32_t qe)
+{
+    spi_flash_trans_t t = {
+        .command = qe_wrsr_command,
+        .mosi_data = qe,
+        .mosi_len = qe_sr_bitwidth,
+        .miso_len = 0,
+    };
+    return chip->host->common_command(chip->host, &t);
+}
+
+esp_err_t spi_flash_common_read_status_16b_rdsr_rdsr2(esp_flash_t* chip, uint32_t* out_sr)
+{
+    uint32_t sr, sr2;
+    esp_err_t ret = spi_flash_common_read_qe_sr(chip, CMD_RDSR2, 8, &sr2);
+    if (ret == ESP_OK) {
+        ret = spi_flash_common_read_qe_sr(chip, CMD_RDSR, 8, &sr);
+    }
+    if (ret == ESP_OK) {
+        *out_sr = (sr & 0xff) | ((sr2 & 0xff) << 8);
+    }
+    return ret;
+}
+
+esp_err_t spi_flash_common_read_status_8b_rdsr2(esp_flash_t* chip, uint32_t* out_sr)
+{
+    return spi_flash_common_read_qe_sr(chip, CMD_RDSR2, 8, out_sr);
+}
+
+esp_err_t spi_flash_common_read_status_8b_rdsr(esp_flash_t* chip, uint32_t* out_sr)
+{
+    return spi_flash_common_read_qe_sr(chip, CMD_RDSR, 8, out_sr);
+}
+
+esp_err_t spi_flash_common_write_status_16b_wrsr(esp_flash_t* chip, uint32_t sr)
+{
+    return spi_flash_common_write_qe_sr(chip, CMD_WRSR, 16, sr);
+}
+
+esp_err_t spi_flash_common_write_status_8b_wrsr(esp_flash_t* chip, uint32_t sr)
+{
+    return spi_flash_common_write_qe_sr(chip, CMD_WRSR, 8, sr);
+}
+
+esp_err_t spi_flash_common_write_status_8b_wrsr2(esp_flash_t* chip, uint32_t sr)
+{
+    return spi_flash_common_write_qe_sr(chip, CMD_WRSR2, 8, sr);
+}
+
+esp_err_t spi_flash_common_set_io_mode(esp_flash_t *chip, esp_flash_wrsr_func_t wrsr_func, esp_flash_rdsr_func_t rdsr_func, uint32_t qe_sr_bit)
+{
+    esp_err_t ret = ESP_OK;
+    const bool is_quad_mode = esp_flash_is_quad_mode(chip);
+    bool update_config = false;
+    const bool force_check = true; //in case some chips doesn't support erase QE
+
+    bool need_check = is_quad_mode;
+    if (force_check) {
+        need_check = true;
+    }
+
+    uint32_t sr_update;
+    if (need_check) {
+        // Ensure quad modes are enabled, using the Quad Enable parameters supplied.
+        uint32_t sr;
+        ret = (*rdsr_func)(chip, &sr);
+        if (ret != ESP_OK) {
+            return ret;
+        }
+        ESP_EARLY_LOGD(TAG, "set_io_mode: status before 0x%x", sr);
+        if (is_quad_mode) {
+            sr_update = sr | qe_sr_bit;
+        } else {
+            sr_update = sr & (~qe_sr_bit);
+        }
+        ESP_EARLY_LOGV(TAG, "set_io_mode: status update 0x%x", sr_update);
+        if (sr != sr_update) {
+            update_config = true;
+        }
+    }
+
+    if (update_config) {
+        //some chips needs the write protect to be disabled before writing to Status Register
+        chip->chip_drv->set_chip_write_protect(chip, false);
+
+        ret = (*wrsr_func)(chip, sr_update);
+        if (ret != ESP_OK) {
+            return ret;
+        }
+
+        ret = chip->chip_drv->wait_idle(chip, DEFAULT_IDLE_TIMEOUT);
+        if (ret != ESP_OK) {
+            return ret;
+        }
+
+        /* Check the new QE bit has stayed set */
+        uint32_t sr;
+        ret = (*rdsr_func)(chip, &sr);
+        if (ret != ESP_OK) {
+            return ret;
+        }
+        ESP_EARLY_LOGD(TAG, "set_io_mode: status after 0x%x", sr);
+        if (sr != sr_update) {
+            ret = ESP_ERR_FLASH_NO_RESPONSE;
+        }
+
+        chip->chip_drv->set_chip_write_protect(chip, true);
+    }
+    return ret;
+}

components/spi_flash/test/test_esp_flash.c
@@ -127,7 +127,7 @@ static void release_bus(int host_id)
     }
 }
 
-static void setup_new_chip(esp_flash_read_mode_t io_mode, esp_flash_speed_t speed)
+static void setup_new_chip(esp_flash_io_mode_t io_mode, esp_flash_speed_t speed)
 {
     //the bus should be initialized before the flash is attached to the bus
     setup_bus(TEST_HOST);

components/soc/esp32/include/hal/spi_flash_ll.h
@@ -202,21 +202,6 @@ static inline bool spi_flash_ll_host_idle(const spi_dev_t *dev)
     return dev->ext2.st != 0;
 }
 
-/**
- * Set phases for user-defined transaction to read
- *
- * @param dev Beginning address of the peripheral registers.
- */
-static inline void spi_flash_ll_read_phase(spi_dev_t *dev)
-{
-    typeof (dev->user) user = {
-        .usr_command = 1,
-        .usr_mosi = 0,
-        .usr_miso = 1,
-        .usr_addr = 1,
-    };
-    dev->user = user;
-}
 /*------------------------------------------------------------------------------
  * Configs
  *----------------------------------------------------------------------------*/
@@ -239,7 +224,7 @@ static inline void spi_flash_ll_set_cs_pin(spi_dev_t *dev, int pin)
  * @param dev Beginning address of the peripheral registers.
  * @param read_mode I/O mode to use in the following transactions.
  */
-static inline void spi_flash_ll_set_read_mode(spi_dev_t *dev, esp_flash_read_mode_t read_mode)
+static inline void spi_flash_ll_set_read_mode(spi_dev_t *dev, esp_flash_io_mode_t read_mode)
 {
     typeof (dev->ctrl) ctrl = dev->ctrl;
     ctrl.val &= ~(SPI_FREAD_QIO_M | SPI_FREAD_QUAD_M | SPI_FREAD_DIO_M | SPI_FREAD_DUAL_M);

components/soc/include/hal/spi_flash_hal.h
@@ -155,23 +155,38 @@ esp_err_t spi_flash_hal_set_write_protect(spi_flash_host_driver_t *chip_drv, boo
 bool spi_flash_hal_host_idle(spi_flash_host_driver_t *driver);
 
 /**
- *  Configure the SPI host hardware registers for the specified read mode.
+ * @brief Configure the SPI host hardware registers for the specified io mode.
  *
  *  Note that calling this configures SPI host registers, so if running any
- *  other commands as part of set_read_mode() then these must be run before
+ *  other commands as part of set_io_mode() then these must be run before
  *  calling this function.
  *
+ *  The command value, address length and dummy cycles are configured according
+ *  to the format of read commands:
+ *
+ *  - command: 8 bits, value set.
+ *  - address: 24 bits
+ *  - dummy: cycles to compensate the input delay
+ *  - out & in data: 0 bits.
+ *
+ *  The following commands still need to:
+ *
+ *  - Read data: set address value and data (length and contents), no need
+ *    to touch command and dummy phases.
+ *  - Common read: set command value, address value (or length to 0 if not used)
+ *  - Common write: set command value, address value (or length to 0 if not
+ *    used), disable dummy phase, and set output data.
+ *
  * @param driver The driver context
- * @param read_mode The HW read mode to use
+ * @param io_mode The HW read mode to use
  * @param addr_bitlen Length of the address phase, in bits
  * @param dummy_cyclelen_base Base cycles of the dummy phase, some extra dummy cycles may be appended to compensate the timing.
- * @param read_command  Actual reading command to send to flash chip on the bus.
+ * @param command  Actual reading command to send to flash chip on the bus.
  *
  * @return always return ESP_OK.
  */
-esp_err_t spi_flash_hal_configure_host_read_mode(spi_flash_host_driver_t *driver, esp_flash_read_mode_t read_mode,
-        uint32_t addr_bitlen, uint32_t dummy_cyclelen_base,
-        uint32_t read_command);
+esp_err_t spi_flash_hal_configure_host_io_mode(spi_flash_host_driver_t *driver, uint32_t command, uint32_t addr_bitlen,
+                                               int dummy_cyclelen_base, esp_flash_io_mode_t io_mode);
 
 /**
  * Poll until the last operation is done.

components/soc/include/hal/spi_flash_types.h
@@ -61,7 +61,7 @@ typedef enum {
     SPI_FLASH_QIO,    ///< Both address & data transferred using quad I/O
 
     SPI_FLASH_READ_MODE_MAX,    ///< The fastest io mode supported by the host is ``ESP_FLASH_READ_MODE_MAX-1``.
-} esp_flash_read_mode_t;
+} esp_flash_io_mode_t;
 
 ///Slowest io mode supported by ESP32, currently SlowRd
 #define SPI_FLASH_READ_MODE_MIN SPI_FLASH_SLOWRD
@@ -130,9 +130,11 @@ struct spi_flash_host_driver_t {
      */
     bool (*host_idle)(spi_flash_host_driver_t *driver);
     /**
-     * Configure the host to work at different read mode.
+     * Configure the host to work at different read mode. Responsible to compensate the timing and set IO mode.
      */
-    esp_err_t (*configure_host_read_mode)(spi_flash_host_driver_t *driver, esp_flash_read_mode_t read_mode, uint32_t addr_bitlen, uint32_t dummy_bitlen_base, uint32_t read_command);
+    esp_err_t (*configure_host_io_mode)(spi_flash_host_driver_t *driver, uint32_t command,
+                                        uint32_t addr_bitlen, int dummy_bitlen_base,
+                                        esp_flash_io_mode_t io_mode);
     /**
      *  Internal use, poll the HW until the last operation is done.
      */

components/soc/src/hal/spi_flash_hal.c
@@ -67,8 +67,3 @@ esp_err_t spi_flash_hal_init(spi_flash_memspi_data_t *data_out, const spi_flash_
     ESP_EARLY_LOGD(TAG, "extra_dummy: %d", data_out->extra_dummy);
     return ESP_OK;
 }
-
-static inline spi_dev_t *get_spi_dev(spi_flash_host_driver_t *chip_drv)
-{
-    return ((spi_flash_memspi_data_t *)chip_drv->driver_data)->spi;
-}

components/soc/src/hal/spi_flash_hal_iram.c
@@ -52,28 +52,37 @@ esp_err_t spi_flash_hal_device_config(spi_flash_host_driver_t *driver)
     return ESP_OK;
 }
 
-esp_err_t spi_flash_hal_configure_host_read_mode(spi_flash_host_driver_t *driver, esp_flash_read_mode_t read_mode,
-        uint32_t addr_bitlen, uint32_t dummy_cyclelen_base,
-        uint32_t read_command)
+esp_err_t spi_flash_hal_configure_host_io_mode(
+    spi_flash_host_driver_t *host,
+    uint32_t command,
+    uint32_t addr_bitlen,
+    int dummy_cyclelen_base,
+    esp_flash_io_mode_t io_mode)
 {
     // Add dummy cycles to compensate for latency of GPIO matrix and external delay, if necessary...
     int dummy_cyclelen = dummy_cyclelen_base + ((spi_flash_memspi_data_t *)driver->driver_data)->extra_dummy;
 
     spi_dev_t *dev = get_spi_dev(driver);
+    spi_flash_ll_set_command8(dev, command);
     spi_flash_ll_set_addr_bitlen(dev, addr_bitlen);
-    spi_flash_ll_set_command8(dev, read_command);
-    spi_flash_ll_read_phase(dev);
     spi_flash_ll_set_dummy(dev, dummy_cyclelen);
-    spi_flash_ll_set_read_mode(dev, read_mode);
+    //disable all data phases, enable them later if needed
+    spi_flash_ll_set_miso_bitlen(dev, 0);
+    spi_flash_ll_set_mosi_bitlen(dev, 0);
+    spi_flash_ll_set_read_mode(dev, io_mode);
     return ESP_OK;
 }
 
 esp_err_t spi_flash_hal_common_command(spi_flash_host_driver_t *chip_drv, spi_flash_trans_t *trans)
 {
-    chip_drv->configure_host_read_mode(chip_drv, SPI_FLASH_FASTRD, 0, 0, 0);
+    host->configure_host_io_mode(host, trans->command, 0, 0, SPI_FLASH_FASTRD);
+
     spi_dev_t *dev = get_spi_dev(chip_drv);
-    spi_flash_ll_set_command8(dev, trans->command);
-    spi_flash_ll_set_addr_bitlen(dev, 0);
+    //disable dummy if no input phase
+    if (trans->miso_len == 0) {
+        spi_flash_ll_set_dummy(dev, 0);
+    }
+
     spi_flash_ll_set_miso_bitlen(dev, trans->miso_len);
     spi_flash_ll_set_mosi_bitlen(dev, trans->mosi_len);
 
@@ -122,7 +131,7 @@ void spi_flash_hal_program_page(spi_flash_host_driver_t *chip_drv, const void *b
 esp_err_t spi_flash_hal_read(spi_flash_host_driver_t *chip_drv, void *buffer, uint32_t address, uint32_t read_len)
 {
     spi_dev_t *dev = get_spi_dev(chip_drv);
-    //the command is already set by ``spi_flash_hal_configure_host_read_mode`` before.
+    //the command is already set by ``spi_flash_hal_configure_host_io_mode`` before.
     spi_flash_ll_set_address(dev, address << 8);
     spi_flash_ll_set_miso_bitlen(dev, read_len * 8);
     spi_flash_ll_user_start(dev);

components/spi_flash/CMakeLists.txt
@@ -3,25 +3,31 @@ if(BOOTLOADER_BUILD)
     # Bootloader needs SPIUnlock from this file, but doesn't
     # need other parts of this component
     set(srcs "spi_flash_rom_patch.c")
+    set(cache_srcs "")
 else()
-    set(srcs
+    set(cache_srcs
         "cache_utils.c"
         "flash_mmap.c"
         "flash_ops.c"
+    )
+    set(srcs
         "partition.c"
         "spi_flash_rom_patch.c"
     )
     # New implementation
+    list(APPEND cache_srcs
+        "esp_flash_api.c"
+        "esp_flash_spi_init.c"
+        "spi_flash_os_func_app.c"
+        "spi_flash_os_func_noos.c"
+    )
     list(APPEND srcs
         "spi_flash_chip_drivers.c"
         "spi_flash_chip_generic.c"
         "spi_flash_chip_issi.c"
-        "spi_flash_os_func_app.c"
-        "spi_flash_os_func_noos.c"
         "memspi_host_driver.c"
-        "esp_flash_api.c"
-        "esp_flash_spi_init.c"
     )
+    list(APPEND srcs ${cache_srcs})
     set(priv_requires bootloader_support app_update soc)
 endif()
 
@@ -30,3 +36,7 @@ idf_component_register(SRCS "${srcs}"
                     INCLUDE_DIRS include
                     PRIV_INCLUDE_DIRS private_include
                     LDFRAGMENTS linker.lf)
+
+# Avoid cache miss by unexpected inlineing when built by -Os
+set_source_files_properties(${cache_srcs} PROPERTIES COMPILE_FLAGS
+    "-fno-inline-functions -fno-inline-small-functions -fno-inline-functions-called-once")

commit f1f9f9ffe3170732d45bb6f0eb21ebb9db5d04d0

esp_flash: fix the regression of non-quad mode by default chip driver
The issue is introduced in 76ff41ad34fbe0f4c07da4076a0b060ac79f200b. The
esp_flash API tries to clear the QE bit when the flash is not working in
quad modes.

However this introduces a regression, compared to earlier versions and
the legacy API. When the chip is not detected, the generic chip driver
is used, which cannot 100% handle the QE bit properly for all flash
vendors. There may be some flash chips (e.g. MXIC) that can be used in
dual modes by legacy API, but output wrong data when the esp_flash API
clears the QE bit in a wrong way.

This commit reverts the QE force clearing behavior, so that it's safer
for the generic chip driver to work under dual modes.

components/spi_flash/spi_flash_chip_drivers.c
@@ -39,6 +39,8 @@ static const spi_flash_chip_t *default_registered_chips[] = {
 #ifdef CONFIG_SPI_FLASH_SUPPORT_MXIC_CHIP
     &esp_flash_chip_mxic,
 #endif
+    // Default chip drivers that will accept all chip ID.
+    // FM, Winbond and XMC chips are supposed to be supported by this chip driver.
     &esp_flash_chip_generic,
     NULL,
 };

components/spi_flash/spi_flash_chip_generic.c
@@ -456,12 +456,15 @@ esp_err_t spi_flash_common_set_io_mode(esp_flash_t *chip, esp_flash_wrsr_func_t 
     esp_err_t ret = ESP_OK;
     const bool is_quad_mode = esp_flash_is_quad_mode(chip);
     bool update_config = false;
-    const bool force_check = true; //in case some chips doesn't support erase QE
+    /*
+     * By default, we don't clear the QE bit even the flash mode is not QIO or QOUT. Force clearing
+     * QE bit by the generic chip driver (command 01H with 2 bytes) may cause the output of some
+     * chips (MXIC) no longer valid.
+     * Enable this option when testing a new flash chip for clearing of QE.
+     */
+    const bool force_check = false;
 
-    bool need_check = is_quad_mode;
-    if (force_check) {
-        need_check = true;
-    }
+    bool need_check = is_quad_mode || force_check;
 
     uint32_t sr_update;
     if (need_check) {

components/spi_flash/test/test_esp_flash.c
@@ -77,6 +77,9 @@ typedef void (*flash_test_func_t)(esp_flash_t* chip);
 #define FLASH_TEST_CASE(STR, FUNC_TO_RUN) \
     TEST_CASE(STR, "[esp_flash]") {flash_test_func(FUNC_TO_RUN, 1 /* first index reserved for main flash */ );}
 
+#define FLASH_TEST_CASE_IGNORE(STR, FUNC_TO_RUN) \
+    TEST_CASE(STR, "[esp_flash][ignore]") {flash_test_func(FUNC_TO_RUN, 1 /* first index reserved for main flash */ );}
+
 /* Use FLASH_TEST_CASE_3 for tests which also run on external flash, which sits in the place of PSRAM
    (these tests are incompatible with PSRAM)
 
@@ -84,10 +87,14 @@ typedef void (*flash_test_func_t)(esp_flash_t* chip);
  */
 #if defined(CONFIG_SPIRAM_SUPPORT) || TEMPORARY_DISABLED_FOR_TARGETS(ESP32S2)
 #define FLASH_TEST_CASE_3(STR, FUNCT_TO_RUN)
+#define FLASH_TEST_CASE_3_IGNORE(STR, FUNCT_TO_RUN)
 #else
 // Disabled for ESP32-S2 due to lack of runners
 #define FLASH_TEST_CASE_3(STR, FUNC_TO_RUN) \
     TEST_CASE(STR", 3 chips", "[esp_flash][test_env=UT_T1_ESP_FLASH]") {flash_test_func(FUNC_TO_RUN, TEST_CONFIG_NUM);}
+
+#define FLASH_TEST_CASE_3_IGNORE(STR, FUNC_TO_RUN) \
+    TEST_CASE(STR", 3 chips", "[esp_flash][test_env=UT_T1_ESP_FLASH][ignore]") {flash_test_func(FUNC_TO_RUN, TEST_CONFIG_NUM);}
 #endif
 
 //currently all the configs are the same with esp_flash_spi_device_config_t, no more information required
@@ -499,8 +506,9 @@ static void read_and_check(esp_flash_t *chip, const esp_partition_t *part, const
 esp_err_t esp_flash_set_io_mode(esp_flash_t* chip, bool qe);
 esp_err_t esp_flash_get_io_mode(esp_flash_t* chip, bool* qe);
 esp_err_t esp_flash_read_chip_id(esp_flash_t* chip, uint32_t* flash_id);
+esp_err_t spi_flash_chip_mxic_probe(esp_flash_t *chip, uint32_t flash_id);
 
-static bool check_winbond_chip(esp_flash_t* chip)
+static bool is_winbond_chip(esp_flash_t* chip)
 {
     uint32_t flash_id;
     esp_err_t ret = esp_flash_read_chip_id(chip, &flash_id);
@@ -510,6 +518,14 @@ static bool check_winbond_chip(esp_flash_t* chip)
     } else {
         return false;
     }
+}
+
+static bool is_mxic_chip(esp_flash_t* chip)
+{
+    uint32_t flash_id;
+    esp_err_t ret = esp_flash_read_chip_id(chip, &flash_id);
+    TEST_ESP_OK(ret);
+    return (spi_flash_chip_mxic_probe(chip, flash_id)==ESP_OK);
 }
 
 static void test_toggle_qe(esp_flash_t* chip)
@@ -522,14 +538,14 @@ static void test_toggle_qe(esp_flash_t* chip)
     esp_err_t ret = esp_flash_get_io_mode(chip, &qe);
     TEST_ESP_OK(ret);
 
-    bool is_winbond_chip = check_winbond_chip(chip);
+    bool allow_failure = is_winbond_chip(chip) || is_mxic_chip(chip);
 
     for (int i = 0; i < 4; i ++) {
         ESP_LOGI(TAG, "write qe: %d->%d", qe, !qe);
         qe = !qe;
         chip->read_mode = qe? SPI_FLASH_QOUT: SPI_FLASH_SLOWRD;
         ret = esp_flash_set_io_mode(chip, qe);
-        if (is_winbond_chip && !qe && ret == ESP_ERR_FLASH_NO_RESPONSE) {
+        if (allow_failure && !qe && ret == ESP_ERR_FLASH_NO_RESPONSE) {
             //allows clear qe failure for Winbond chips
             ret = ESP_OK;
         }
@@ -539,8 +555,12 @@ static void test_toggle_qe(esp_flash_t* chip)
         ret = esp_flash_get_io_mode(chip, &qe_read);
         TEST_ESP_OK(ret);
         ESP_LOGD(TAG, "qe read: %d", qe_read);
-        if (qe != qe_read && !qe && is_winbond_chip) {
-            ESP_LOGE(TAG, "cannot clear QE bit, this may be normal for Winbond chips.");
+        if (!qe && qe_read) {
+            if (allow_failure) {
+                ESP_LOGW(TAG, "cannot clear QE bit for known permanent QE (Winbond or MXIC) chips.");
+            } else {
+                ESP_LOGE(TAG, "cannot clear QE bit, please make sure force clearing QE option is enabled in `spi_flash_common_set_io_mode`, and this chip is not a permanent QE one.");
+            }
             chip->read_mode = io_mode_before;
             return;
         }
@@ -550,8 +570,11 @@ static void test_toggle_qe(esp_flash_t* chip)
     chip->read_mode = io_mode_before;
 }
 
-FLASH_TEST_CASE("Test esp_flash_write can toggle QE bit", test_toggle_qe);
-FLASH_TEST_CASE_3("Test esp_flash_write can toggle QE bit", test_toggle_qe);
+// These tests show whether the QE is permanent or not for the chip tested.
+// To test the behaviour of a new SPI flash chip, enable force_check flag in generic driver
+// `spi_flash_common_set_io_mode` and then run this test.
+FLASH_TEST_CASE_IGNORE("Test esp_flash_write can toggle QE bit", test_toggle_qe);
+FLASH_TEST_CASE_3_IGNORE("Test esp_flash_write can toggle QE bit", test_toggle_qe);
 
 
 void test_permutations(flashtest_config_t* config)