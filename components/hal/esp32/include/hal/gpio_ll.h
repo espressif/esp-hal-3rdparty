espressif/esp-hal-3rdparty

commit 91166b70ccb9a0a33a180b553f52488420abd4d4

soc/ll: workaround compiler bug that generate 8/16 bits inst instead of 32 bits one
update all struct headers to be more "standardized":

- bit fields are properly wrapped with struct
- bitwidth sum should be 32 within same struct, so that it's correctly padded with reserved bits
- bit field should be uint32_t
- typedef volatile struct xxx{} yyy;: xxx must exists. refer: [Github: espressif/esp-idf]/pull/3199

added helper macros to force peripheral registers being accessed in 32 bitwidth

added a check script into ci

components/hal/esp32/include/hal/gpio_ll.h
@@ -25,9 +25,11 @@
 #include <stdbool.h>
 #include "soc/soc.h"
 #include "soc/gpio_periph.h"
+#include "soc/gpio_struct.h"
 #include "soc/rtc_cntl_reg.h"
 #include "soc/rtc_io_reg.h"
 #include "hal/gpio_types.h"
+#include "hal/misc.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -245,7 +247,7 @@ static inline void gpio_ll_get_intr_status(gpio_dev_t *hw, uint32_t core_id, uin
   */
 static inline void gpio_ll_get_intr_status_high(gpio_dev_t *hw, uint32_t core_id, uint32_t *status)
 {
-    *status = (core_id == 0) ? hw->pcpu_int1.intr : hw->acpu_int1.intr;
+    *status = (core_id == 0) ? HAL_FORCE_READ_U32_REG_FIELD(hw->pcpu_int1, intr) : HAL_FORCE_READ_U32_REG_FIELD(hw->pcpu_int1, intr);
 }
 
 /**
@@ -267,7 +269,7 @@ static inline void gpio_ll_clear_intr_status(gpio_dev_t *hw, uint32_t mask)
   */
 static inline void gpio_ll_clear_intr_status_high(gpio_dev_t *hw, uint32_t mask)
 {
-    hw->status1_w1tc.intr_st = mask;
+    HAL_FORCE_MODIFY_U32_REG_FIELD(hw->status1_w1tc, intr_st, mask);
 }
 
 /**
@@ -330,7 +332,7 @@ static inline void gpio_ll_output_disable(gpio_dev_t *hw, gpio_num_t gpio_num)
     if (gpio_num < 32) {
         hw->enable_w1tc = (0x1 << gpio_num);
     } else {
-        hw->enable1_w1tc.data = (0x1 << (gpio_num - 32));
+        HAL_FORCE_MODIFY_U32_REG_FIELD(hw->enable1_w1tc, data, (0x1 << (gpio_num - 32)));
     }
 
     // Ensure no other output signal is routed via GPIO matrix to this pin
@@ -349,7 +351,7 @@ static inline void gpio_ll_output_enable(gpio_dev_t *hw, gpio_num_t gpio_num)
     if (gpio_num < 32) {
         hw->enable_w1ts = (0x1 << gpio_num);
     } else {
-        hw->enable1_w1ts.data = (0x1 << (gpio_num - 32));
+        HAL_FORCE_MODIFY_U32_REG_FIELD(hw->enable1_w1ts, data, (0x1 << (gpio_num - 32)));
     }
 }
 
@@ -432,13 +434,13 @@ static inline void gpio_ll_set_level(gpio_dev_t *hw, gpio_num_t gpio_num, uint32
         if (gpio_num < 32) {
             hw->out_w1ts = (1 << gpio_num);
         } else {
-            hw->out1_w1ts.data = (1 << (gpio_num - 32));
+            HAL_FORCE_MODIFY_U32_REG_FIELD(hw->out1_w1ts, data, (1 << (gpio_num - 32)));
         }
     } else {
         if (gpio_num < 32) {
             hw->out_w1tc = (1 << gpio_num);
         } else {
-            hw->out1_w1tc.data = (1 << (gpio_num - 32));
+            HAL_FORCE_MODIFY_U32_REG_FIELD(hw->out1_w1tc, data, (1 << (gpio_num - 32)));
         }
     }
 }
@@ -460,7 +462,7 @@ static inline int gpio_ll_get_level(gpio_dev_t *hw, gpio_num_t gpio_num)
     if (gpio_num < 32) {
         return (hw->in >> gpio_num) & 0x1;
     } else {
-        return (hw->in1.data >> (gpio_num - 32)) & 0x1;
+        return (HAL_FORCE_READ_U32_REG_FIELD(hw->in1, data) >> (gpio_num - 32)) & 0x1;
     }
 }
 

components/hal/esp32c3/include/hal/gpio_ll.h
@@ -24,6 +24,7 @@
 
 #include "soc/soc.h"
 #include "soc/gpio_periph.h"
+#include "soc/gpio_struct.h"
 #include "soc/rtc_cntl_reg.h"
 #include "hal/gpio_types.h"
 #include "stdlib.h"

components/hal/esp32h2/include/hal/gpio_ll.h
@@ -25,6 +25,7 @@
 #include "soc/soc.h"
 #include "soc/gpio_periph.h"
 #include "soc/rtc_cntl_reg.h"
+#include "soc/gpio_struct.h"
 #include "hal/gpio_types.h"
 #include "stdlib.h"
 

components/hal/esp32s2/include/hal/gpio_ll.h
@@ -26,6 +26,7 @@
 #include "soc/gpio_periph.h"
 #include "soc/rtc_cntl_reg.h"
 #include "soc/rtc_io_reg.h"
+#include "soc/gpio_struct.h"
 #include "hal/gpio_types.h"
 
 #ifdef __cplusplus

components/hal/esp32s3/include/hal/gpio_ll.h
@@ -28,6 +28,7 @@
 #include "soc/rtc_io_reg.h"
 #include "soc/usb_serial_jtag_reg.h"
 #include "hal/gpio_types.h"
+#include "soc/gpio_struct.h"
 
 #ifdef __cplusplus
 extern "C" {

commit 1e3fadcdae68f286ff9330467b5794d54e7fbda3

gpio: Fix the bug that esp32 gpio interrupt cannot be triggered on core 1
Introduced in 91166b70ccb9a0a33a180b553f52488420abd4d4

Closes [Github: espressif/esp-idf]/issues/7594

components/hal/esp32/include/hal/gpio_ll.h
@@ -239,7 +239,7 @@ static inline void gpio_ll_get_intr_status(gpio_dev_t *hw, uint32_t core_id, uin
   */
 static inline void gpio_ll_get_intr_status_high(gpio_dev_t *hw, uint32_t core_id, uint32_t *status)
 {
-    *status = (core_id == 0) ? HAL_FORCE_READ_U32_REG_FIELD(hw->pcpu_int1, intr) : HAL_FORCE_READ_U32_REG_FIELD(hw->pcpu_int1, intr);
+    *status = (core_id == 0) ? HAL_FORCE_READ_U32_REG_FIELD(hw->pcpu_int1, intr) : HAL_FORCE_READ_U32_REG_FIELD(hw->acpu_int1, intr);
 }
 
 /**