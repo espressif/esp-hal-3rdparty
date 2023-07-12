ee8052d9c23c2bcb1a3768a5141144713a1a819e
Lucas Saavedra Vaz <lucas.vaz@espressif.com>
2023-07-12 17:18:24 -0300

    nuttx: Add clk_ctrl_os.c port

--- components/esp_hw_support/clk_ctrl_os.c
+++ components/esp_hw_support/clk_ctrl_os.c
@@ -4,13 +4,33 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#ifdef __NuttX__
+#include <nuttx/spinlock.h>
+#else
 #include <freertos/FreeRTOS.h>
+#endif
+
 #include "clk_ctrl_os.h"
 #include "soc/rtc.h"
 #include "esp_private/esp_clk_tree_common.h"
 #include "esp_check.h"
 
-static portMUX_TYPE periph_spinlock = portMUX_INITIALIZER_UNLOCKED;
+#ifdef __NuttX__
+#define ENTER_CRITICAL_SECTION(lock)    do { g_flags = spin_lock_irqsave(lock); } while(0)
+#define LEAVE_CRITICAL_SECTION(lock)    spin_unlock_irqrestore((lock), g_flags)
+#define LOCK_INITIALIZER_UNLOCKED       0
+
+typedef spinlock_t lock_type_t;
+static irqstate_t g_flags;
+#else
+#define ENTER_CRITICAL_SECTION(lock)    portENTER_CRITICAL_SAFE(lock)
+#define LEAVE_CRITICAL_SECTION(lock)    portEXIT_CRITICAL_SAFE(lock)
+#define LOCK_INITIALIZER_UNLOCKED       portMUX_INITIALIZER_UNLOCKED
+
+typedef portMUX_TYPE lock_type_t;
+#endif
+
+static lock_type_t periph_spinlock = LOCK_INITIALIZER_UNLOCKED;
 
 static uint8_t s_periph_ref_counts = 0;
 static uint32_t s_rc_fast_freq = 0; // Frequency of the RC_FAST clock in Hz
@@ -24,20 +44,20 @@ static int s_apll_ref_cnt = 0;
 
 bool periph_rtc_dig_clk8m_enable(void)
 {
-    portENTER_CRITICAL(&periph_spinlock);
+    ENTER_CRITICAL_SECTION(&periph_spinlock);
     if (s_periph_ref_counts == 0) {
         rtc_dig_clk8m_enable();
 #if SOC_CLK_RC_FAST_SUPPORT_CALIBRATION
         s_rc_fast_freq = esp_clk_tree_rc_fast_get_freq_hz(ESP_CLK_TREE_SRC_FREQ_PRECISION_EXACT);
         if (s_rc_fast_freq == 0) {
             rtc_dig_clk8m_disable();
-            portEXIT_CRITICAL(&periph_spinlock);
+            LEAVE_CRITICAL_SECTION(&periph_spinlock);
             return false;
         }
 #endif //SOC_CLK_RC_FAST_SUPPORT_CALIBRATION
     }
     s_periph_ref_counts++;
-    portEXIT_CRITICAL(&periph_spinlock);
+    LEAVE_CRITICAL_SECTION(&periph_spinlock);
     return true;
 }
 
@@ -53,31 +73,31 @@ uint32_t periph_rtc_dig_clk8m_get_freq(void)
 
 void periph_rtc_dig_clk8m_disable(void)
 {
-    portENTER_CRITICAL(&periph_spinlock);
+    ENTER_CRITICAL_SECTION(&periph_spinlock);
     assert(s_periph_ref_counts > 0);
     s_periph_ref_counts--;
     if (s_periph_ref_counts == 0) {
         s_rc_fast_freq = 0;
         rtc_dig_clk8m_disable();
     }
-    portEXIT_CRITICAL(&periph_spinlock);
+    LEAVE_CRITICAL_SECTION(&periph_spinlock);
 }
 
 #if SOC_CLK_APLL_SUPPORTED
 void periph_rtc_apll_acquire(void)
 {
-    portENTER_CRITICAL(&periph_spinlock);
+    ENTER_CRITICAL_SECTION(&periph_spinlock);
     s_apll_ref_cnt++;
     if (s_apll_ref_cnt == 1) {
         // For the first time enable APLL, need to set power up
         rtc_clk_apll_enable(true);
     }
-    portEXIT_CRITICAL(&periph_spinlock);
+    LEAVE_CRITICAL_SECTION(&periph_spinlock);
 }
 
 void periph_rtc_apll_release(void)
 {
-    portENTER_CRITICAL(&periph_spinlock);
+    ENTER_CRITICAL_SECTION(&periph_spinlock);
     assert(s_apll_ref_cnt > 0);
     s_apll_ref_cnt--;
     if (s_apll_ref_cnt == 0) {
@@ -85,7 +105,7 @@ void periph_rtc_apll_release(void)
         s_cur_apll_freq = 0;
         rtc_clk_apll_enable(false);
     }
-    portEXIT_CRITICAL(&periph_spinlock);
+    LEAVE_CRITICAL_SECTION(&periph_spinlock);
 }
 
 esp_err_t periph_rtc_apll_freq_set(uint32_t expt_freq, uint32_t *real_freq)
@@ -100,7 +120,7 @@ esp_err_t periph_rtc_apll_freq_set(uint32_t expt_freq, uint32_t *real_freq)
 
     ESP_RETURN_ON_FALSE(apll_freq, ESP_ERR_INVALID_ARG, TAG, "APLL coefficients calculate failed");
     bool need_config = true;
-    portENTER_CRITICAL(&periph_spinlock);
+    ENTER_CRITICAL_SECTION(&periph_spinlock);
     /* If APLL is not in use or only one peripheral in use, its frequency can be changed as will
      * But when more than one peripheral refers APLL, its frequency is not allowed to change once it is set */
     if (s_cur_apll_freq == 0 || s_apll_ref_cnt < 2) {
@@ -109,7 +129,7 @@ esp_err_t periph_rtc_apll_freq_set(uint32_t expt_freq, uint32_t *real_freq)
         apll_freq = s_cur_apll_freq;
         need_config = false;
     }
-    portEXIT_CRITICAL(&periph_spinlock);
+    LEAVE_CRITICAL_SECTION(&periph_spinlock);
     *real_freq = apll_freq;
 
     if (need_config) {
