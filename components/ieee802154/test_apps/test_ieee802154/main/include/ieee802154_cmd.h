66e797e39e59484e8dc2102463e2960c1438750e
Xu Si Yu <xusiyu@espressif.com>
2023-10-11 19:41:20 +0800

    feat(ieee802154): Add ieee802154 CLI example

--- components/ieee802154/test_apps/test_ieee802154/main/include/ieee802154_cmd.h
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
- *
- * SPDX-License-Identifier: Unlicense OR CC0-1.0
- */
-
-#pragma once
-
-void register_ieee802154_cmd(void);
