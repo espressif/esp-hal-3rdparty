2ac0f4a5d3dc990cf75876b6a1a4a4435d7cc66c
laokaiyao <laokaiyao@espressif.com>
2023-11-07 21:06:07 +0800

    refactor(i2s): make i2s driver as component

---
+++ components/upper_hal_i2s/test_apps/i2s_multi_dev/pytest_i2s_multi_dev.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Unlicense OR CC0-1.0
+import pytest
+
+
+@pytest.mark.esp32s3
+@pytest.mark.esp32c3
+@pytest.mark.esp32c6
+@pytest.mark.esp32h2
+@pytest.mark.generic_multi_device
+@pytest.mark.parametrize('count', [
+    2,
+], indirect=True)
+def test_i2s_multi_dev(case_tester) -> None:  # type: ignore
+    case_tester.run_all_multi_dev_cases(reset=True, timeout=30)
