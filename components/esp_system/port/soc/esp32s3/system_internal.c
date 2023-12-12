2f0e359fa56b6408ccc4e9f27818b64011133ddd
Tiago Medicci Serrano <tiago.medicci@gmail.com>
2023-12-12 17:45:16 -0300

    Provide support for implementing the RMT driver on ESP32-S3.

--- components/esp_system/port/soc/esp32s3/system_internal.c
+++ components/esp_system/port/soc/esp32s3/system_internal.c
@@ -21,8 +21,7 @@
 #include "soc/syscon_reg.h"
 #include "soc/rtc_periph.h"
 #include "hal/wdt_hal.h"
-#include "freertos/xtensa_api.h"
-#include "soc/soc_memory_layout.h"
+#include "xtensa/xtensa_api.h"
 
 #include "esp32s3/rom/cache.h"
 #include "esp32s3/rom/rtc.h"
