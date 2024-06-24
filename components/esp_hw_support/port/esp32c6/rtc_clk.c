espressif/esp-hal

change(esp_hw_support): update xtal_freq after assume to avoid mass print in DFS

components/esp_hw_support/port/esp32c6/rtc_clk.c
@@ -362,6 +362,7 @@ soc_xtal_freq_t rtc_clk_xtal_freq_get(void)
     uint32_t xtal_freq_mhz = clk_ll_xtal_load_freq_mhz();
     if (xtal_freq_mhz == 0) {
         ESP_HW_LOGW(TAG, "invalid RTC_XTAL_FREQ_REG value, assume 40MHz");
+        clk_ll_xtal_store_freq_mhz(SOC_XTAL_FREQ_40M);
         return SOC_XTAL_FREQ_40M;
     }
     return (soc_xtal_freq_t)xtal_freq_mhz;

espressif/esp-hal-3rdparty

components/esp_hw_support/port/esp32c6/rtc_clk.c
@@ -362,6 +362,7 @@ rtc_xtal_freq_t rtc_clk_xtal_freq_get(void)
     uint32_t xtal_freq_mhz = clk_ll_xtal_load_freq_mhz();
     if (xtal_freq_mhz == 0) {
         ESP_HW_LOGW(TAG, "invalid RTC_XTAL_FREQ_REG value, assume 40MHz");
+        clk_ll_xtal_store_freq_mhz(RTC_XTAL_FREQ_40M);
         return RTC_XTAL_FREQ_40M;
     }
     return (rtc_xtal_freq_t)xtal_freq_mhz;