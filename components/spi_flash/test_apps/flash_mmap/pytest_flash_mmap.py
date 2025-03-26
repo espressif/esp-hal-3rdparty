espressif/esp-idf

ci(spi_flash): add tests for cache2phys with XIP

components/spi_flash/test_apps/flash_mmap/pytest_flash_mmap.py
@@ -34,21 +34,20 @@ def test_flash_mmap_rom_impl(dut: Dut) -> None:
     dut.run_all_single_board_cases(timeout=30)
 
 
-XIP_CONFIGS = [
-    pytest.param('xip_psram_esp32s2', marks=[pytest.mark.esp32s2]),
-    pytest.param('xip_psram_esp32s3', marks=[pytest.mark.esp32s3]),
-    pytest.param('xip_psram_esp32c5', marks=[pytest.mark.esp32c5]),
-    pytest.param('xip_psram_esp32c61', marks=[pytest.mark.esp32c61]),
-]
-
-
+@pytest.mark.supported_targets
 @pytest.mark.generic
-@pytest.mark.parametrize('config', XIP_CONFIGS, indirect=True)
+@pytest.mark.parametrize(
+    'config',
+    [
+        'xip_psram',
+    ],
+    indirect=True,
+)
 def test_flash_mmap_xip_psram(dut: Dut) -> None:
     dut.run_all_single_board_cases(timeout=30)
 
 
-@pytest.mark.esp32s3
+@pytest.mark.supported_targets
 @pytest.mark.generic
 @pytest.mark.parametrize(
     'config',

espressif/esp-hal-3rdparty

components/spi_flash/test_apps/flash_mmap/pytest_flash_mmap.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
-
 import pytest
 from pytest_embedded import Dut
 
@@ -35,14 +34,16 @@ def test_flash_mmap_rom_impl(dut: Dut) -> None:
     dut.run_all_single_board_cases(timeout=30)
 
 
-XIP_CONFIGS = [
-    pytest.param('xip_psram_esp32s2', marks=[pytest.mark.esp32s2]),
-    pytest.param('xip_psram_esp32s3', marks=[pytest.mark.esp32s3]),
-]
-
-
+@pytest.mark.esp32s2
+@pytest.mark.esp32s3
 @pytest.mark.generic
-@pytest.mark.parametrize('config', XIP_CONFIGS, indirect=True)
+@pytest.mark.parametrize(
+    'config',
+    [
+        'xip_psram',
+    ],
+    indirect=True,
+)
 def test_flash_mmap_xip_psram(dut: Dut) -> None:
     dut.run_all_single_board_cases(timeout=30)
 