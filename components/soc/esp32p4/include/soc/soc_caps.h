1ad893d4b7f62a056abbd171a8933ca4c6a7a86e
Tomas Rezucha <tomas.rezucha@espressif.com>
2026-04-17 13:47:12 +0200

    Revert "fix(esp_hw_support): Fix deepsleep leakage after initializing USB HS phy"
    This reverts commit df540ef5eed71510ca648350a60c51d9d087f4ad.

--- components/soc/esp32p4/include/soc/soc_caps.h
+++ components/soc/esp32p4/include/soc/soc_caps.h
@@ -434,7 +434,6 @@
 
 // USB PHY Caps
 #define SOC_USB_UTMI_PHY_NUM            (1U)
-#define SOC_USB_UTMI_PHY_NO_POWER_OFF_ISO    1
 
 /*-------------------------- PARLIO CAPS --------------------------------------*/
 #define SOC_PARLIO_TX_UNIT_MAX_DATA_WIDTH    16  /*!< Number of data lines of the TX unit */
