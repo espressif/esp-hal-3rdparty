bd40bbb0ad8a68088b3566f1a9b798409c316e55
Wang Mengyang <wangmengyang@espressif.com>
2026-04-17 15:47:46 +0800

    fix(esp_coex): Disable coexist build for ESP32-S31 before it is supported
    - Disable blufi example build on ESP32-S31

--- components/soc/esp32s31/include/soc/soc_caps.h
+++ components/soc/esp32s31/include/soc/soc_caps.h
@@ -494,6 +494,7 @@
 #define SOC_BLE_POWER_CONTROL_SUPPORTED             (1)     /*!< Support Bluetooth Power Control */
 #define SOC_BLE_MULTI_CONN_OPTIMIZATION             (1)     /*!< Support multiple connections optimization */
 #define SOC_BLE_PERIODIC_ADV_ENH_SUPPORTED          (1)     /*!< Support For BLE Periodic Adv Enhancements */
+// #define SOC_BLUFI_SUPPORTED                         (1)  // TODO: enable this feature coexistence is supported
 #define SOC_BLE_CTE_SUPPORTED                       (1)     /*!< Support Bluetooth LE Constant Tone Extension (CTE) */
 #define SOC_BLE_SUBRATE_SUPPORTED                   (1)     /*!< Support Bluetooth LE Connection Subrating */
 #define SOC_BLE_PERIODIC_ADV_WITH_RESPONSE          (1)     /*!< Support Bluetooth LE Periodic Advertising with Response (PAwR) */
